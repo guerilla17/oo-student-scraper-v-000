aa38753b678c15d2665809640ed2048b42fee6bb
Gigi M <mmgamao1@gmail.com>
2019-07-14 20:10:25 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -18,7 +18,7 @@ end
  def self.create_from_collection(students_array)
     students_array.each { |student_hash| self.new(student_hash) }
   end	  
-  end
+  
 
   def add_student_attributes(attributes_hash)
     @all 
